cb0285929555f4bef1f99d4945cfe0a5658ef384
KunyuanWang <kwan0061@student.monash.edu>
2021-08-26 20:39:08 +0800

    first time

---
+++ .bash_history
@@ -0,0 +1,4 @@
+ls
+sudo /opt/bitnami/apps/wordpress/bnconfig.disabled --disable_banner 1
+sudo /opt/bitnami/bncert-tool
+enter
---
+++ stack/varnish/include/varnish/vmod_abi.h
@@ -0,0 +1,7 @@
+/*
+ * NB:  This file is machine generated, DO NOT EDIT!
+ *
+ * Edit and run include/generate.py instead.
+ */
+
+#define VMOD_ABI_Version "Varnish 6.5.1 1dae23376bb5ea7a6b8e9e4b9ed95cdc9469fb64"
